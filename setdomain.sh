d8ed1c964a0993067f3d7c5b32d1f387658e5c79
Ross Brigoli <ross@Rosss-Mac-Studio.local>
2023-04-02 02:43:10 +0800

    correction on setdomain script

--- setdomain.sh
+++ setdomain.sh
@@ -7,7 +7,7 @@ sudo sed -i 's/home.lab/'$2'/' db.192.168.1
 sudo sed -i 's/okd.home.lab/'$1.$2'/' db.home.lab
 sudo sed -i 's/home.lab/'$2'/' db.home.lab
 sudo sed -i 's/home.lab/'$2'/' db.192.168.1
-sudo mv db.home.lab.new db.$2
+sudo mv db.home.lab db.$2
 sudo sed -i 's/home.lab/'$2'/' named.conf.local
 
 # Replace dns references in install_config.yaml
